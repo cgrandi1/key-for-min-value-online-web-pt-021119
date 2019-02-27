a65a904c57c63e765efb50fb1aaded6a477f209f
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-27 03:04:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,5 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each {key, key_2| puts key if key < key_2}
+  name_hash.each {key, b| puts key if key < b}
 end
